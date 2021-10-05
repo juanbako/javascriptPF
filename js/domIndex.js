6c48c90229acbd701c86bdb5390d3ead74f5d52e
Juan Baro <juanmbaro@gmail.com>
2021-10-05 19:24:12 -0300

    complementario clase 9 - Eventos

--- js/domIndex.js
+++ js/domIndex.js
@@ -1,9 +1,97 @@
-const botonNuevoEspacio = document.getElementById("cargarNuevoEspacio")
+class MoldeParque {
+    constructor (nombre, ubicacion, accesibilidad, tamanio, juegos, maqEjercicios, banios, rejas, canil) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion; //direccion 
+        this.acceso = accesibilidad; //publico o privado
+        this.tamanioHec = tamanio; //en hectareas
+        this.instalaciones = { juegos: juegos, maqEjercicios: maqEjercicios, banios: banios };
+        this.rejas = rejas;
+        this.canil = canil;
+        this.puntuacionParque = 0
+    }
 
-const colaborar = prompt("Deseas cargar un nuevo espacio? s/n")
+    puntuacion () {
+        if (this.acceso === "publico") {this.puntuacionParque++};
+        if (this.instalaciones.juegos === "si") {this.puntuacionParque++};
+        if (this.instalaciones.maqEjercicios === "si" ) {this.puntuacionParque++};
+        if (this.instalaciones.banios === "si") {this.puntuacionParque++};
+        if (this.rejas === "si") {this.puntuacionParque--};
+        if (this.canil === "si") {this.puntuacionParque++};
 
-if (colaborar === "s") {
-    botonNuevoEspacio.classList.remove("cargarNuevoEspacioNone")
-    botonNuevoEspacio.classList.add("cargarNuevoEspacioInline")
+        parseFloat(this.tamanioHec);
+        parseFloat(this.puntuacionParque);
+        
+        return (this.puntuacionParque = parseFloat(this.puntuacionParque) + parseFloat(this.tamanioHec));
+    };
 };
+
+//---ARRAYS
+
+espacios = [];
+
+espacios.push(new MoldeParque ("Avellaneda", "Av. Directorio y Av. Lacarra", "publico", 10, "si", "si", "no", "si", "no"));
+
+//---MODAL
+
+const btnNuevoEspacio = document.getElementById("cargarNuevoEspacio");
+const cargarModal = document.getElementById("cargarNModal");
+const cerrarModalNEspacio = document.getElementById("NespacioCargado");
+const formulario = document.getElementById("formulario")
+const buscarEspacio = document.getElementById("buscarEspacios");
+
+
+btnNuevoEspacio.addEventListener(`click`, () => {
+    cargarModal.classList.add("modalVisible");
+});
+
+//---FORMULARIO NUEVO ESPACIO
+
+const nombreEspacio = document.getElementById("inpNombreEspacio")
+const ubicacionEspacio = document.getElementById("inpUbicacion")
+const tamanio = document.getElementById("inpTamanio")
+const acceso = document.getElementById("acceso")
+const juegos = document.getElementById("juegos")
+const maqEjercicios = document.getElementById("maqEjercicios")
+const banios = document.getElementById("banio")
+const rejas = document.getElementById("rejas")
+const canil = document.getElementById("canil")
+
+
+//---VALIDACIONES NUEVO ESPACIO
+
+function valNombre () {
+    const nombre = nombreEspacio.value
+    if (nombre.length > 2) {
+        nombreEspacio.style.backgroundColor = `rgba(16, 148, 67, 0.6)`;
+    } else {
+        nombreEspacio.style.backgroundColor = `rgba(253, 7, 7, 0.8)`;
+    }
+}
+
+nombreEspacio.addEventListener(`blur`, valNombre);
+
+function valUbicacion () {
+    const ubicacion = ubicacionEspacio.value
+    if (ubicacion.length > 2) {
+        ubicacionEspacio.style.backgroundColor = `rgba(16, 148, 67, 0.6)`;
+    } else {
+        ubicacionEspacio.style.backgroundColor = `rgba(253, 7, 7, 0.8)`;
+    }
+}
+
+ubicacionEspacio.addEventListener(`blur`, valUbicacion);
+
+function valTamanio () {
+    const tamanioF = tamanio.value
+    if (tamanioF >= 1) {
+        tamanio.style.backgroundColor = `rgba(16, 148, 67, 0.6)`;
+    } else {
+        tamanio.style.backgroundColor = `rgba(253, 7, 7, 0.8)`;
+    }
+}
+
+tamanio.addEventListener(`blur`, valTamanio);
+
+
+
 
--- js/domNEspacio.js
+++ js/domNEspacio.js
@@ -1,17 +1,26 @@
-let nombreParque = prompt("Cual es el nombre del nuevo parque?")
-let ubicacionParque = prompt("En que direccion esta?")
-let datosParque = prompt("Alguna caracteristica que quieras sumar?")
+espacios.push(new MoldeParque ("Plaza Velez Sarfield", "Bahia Blaca y Av. Avellaneda", "publico", 1, "si", "si", "no", "si", "no"));
+espacios.push(new MoldeParque ("Plaza Arenales", "Chivilcoy y Nueva York", "publico", 4, "si", "si", "no", "no", "no"));
+espacios.push(new MoldeParque ("Agronomia", "Av. de los Constituyentes y Baigorria", "publico", 16, "si", "si", "si", "si", "si"));
+espacios.push(new MoldeParque ("Parque Lezama", "Av. Martin Garcia y Av. Paseo Colon", "publico", 5, "si", "si", "si", "no", "no"));
 
 const nuevoEspacio = document.getElementById("nuevoEspacio")
 
-nuevoEspacio.innerHTML = `<div class="col-md-3 card shadow-lg p-3 mb-5 bg-body rounded parques__cards--margin">
-                            <img src="./imagenes/nuevoParque.jpg" class="card-img-top" alt="parque">
+espacios.forEach(espacio => {
+    const cardEspacios = document.createElement (`div`)
+    cardEspacios.className = `col-md-3 card shadow-lg p-3 mb-5 bg-body rounded parques__cards--margin`
+    cardEspacios.style = `widht: 18rem`
+
+    cardEspacios.innerHTML = `<img src="./imagenes/nuevoParque.jpg" class="card-img-top" alt="parque">
                             <div class="card-body">
-                                <h5 class="card-title">${nombreParque}</h5>
-                                <p class="card-text">${ubicacionParque}</p>
-                                <p class="card-text">${datosParque}</p>
+                                <h5 class="card-title">${espacio.nombre}</h5>
+                                <p class="card-text">${espacio.ubicacion}</p>
+                                <p class="card-text">${espacio.acceso}</p>
                                 <a href="#" class="btn btn-success">+Info</a>
                             </div>
                         </div>`
 
-//comillas para template strings alt+96
+    nuevoEspacio.appendChild(cardEspacios)
+});
+
+
+
